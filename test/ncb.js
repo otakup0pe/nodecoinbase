493fdf86e2b10636454723afcaa0776b91224f52
Jonathan Freedman <jonafree@gmail.com>
2013-02-13 20:12:09 -0800

    clean up tests some

--- test/ncb.js
+++ test/ncb.js
@@ -2,12 +2,22 @@ var sinon = require('sinon');
 var assert = require('assert');
 var ncb = require('../ncb');
 var nock = require('nock');
+var _ = require('underscore');
+
+function nockit(method, suffix, body) {
+    var scope = nock('https://coinbase.com')
+        .filteringPath(/api_key=[^&]*/g, 'api_key=XXX');
+    if ( _.isUndefined(body) ) {
+        scope = scope.intercept('/api/v1/' + suffix + '?api_key=XXX', method);
+    } else {
+        scope = scope.intercept('/api/v1/' + suffix + '?api_key=XXX', method, body);
+    }
+    return scope;
+}
 
 describe('balance', function() {
              it('should return a number', function(done) {
-                    var scope = nock('https://coinbase.com')
-                        .filteringPath(/api_key=[^&]*/g, 'api_key=XXX')
-                        .get('/api/v1/account/balance?api_key=XXX')
+                    var scope = nockit('GET', 'account/balance')
                         .reply(200, {currency:'BTC', amount:42});
                     return ncb.balance('foo', function(err, balance) {
                                            if ( err ) { throw err; }
@@ -16,9 +26,8 @@ describe('balance', function() {
                                        });
                 });
              it('should throw 401 on bad key', function(done) {
-                    var scope = nock('https://coinbase.com')
-                        .filteringPath(/api_key=[^&]*/g, 'api_key=XXX')
-                        .get('/api/v1/account/balance?api_key=XXX')
+                    
+                    var scope = nockit('GET', 'account/balance')
                         .reply(401);
                     return ncb.balance('foo', function(err, balance) {
                                            assert(err == 'invalid status 401 received');
@@ -28,19 +37,20 @@ describe('balance', function() {
          });
 
 describe('send_btc', function() {
+             var t_to = 'foo@bar.com';
+             var t_amount = 42;
+             var t_note = 'doin thangs';
+             var n_body = {
+                 transaction :
+                 {
+                     to: t_to,
+                     amount: t_amount,
+                     note: t_note
+                 }
+             };
              it('should return a transaction id', function(done) {
-                    var scope = nock('https://coinbase.com')
-                        .filteringPath(/api_key=[^&]*/g, 'api_key=XXX')
-                        .post('/api/v1/transactions/send_money?api_key=XXX',
-                              {
-                                  transaction :
-                                  {
-                                      to: 'foo@bar.com',
-                                      amount: 42,
-                                      note: 'doin thangs'
-                                  }
-                              })
-                        .reply(200,
+                    var scope = nockit('POST', 'transactions/send_money', n_body);
+                        scope.reply(200,
                                {
                                    success:true,
                                    transaction :
@@ -48,20 +58,18 @@ describe('send_btc', function() {
                                        id: "42"
                                    }
                                });
-                    return ncb.send_btc('foo', 'foo@bar.com', 42, 'doin thangs', function(err, id) {
+                    return ncb.send_btc('foo', t_to, t_amount, t_note, function(err, id) {
                                             if ( err ) { throw err; }
                                             assert(id == '42');
                                             done();
                                         });
                 });
              it('should throw 401 on bad key', function(done) {
-                    var scope = nock('https://coinbase.com')
-                        .filteringPath(/api_key=[^&]*/g, 'api_key=XXX')
-                        .get('/api/v1/account/balance?api_key=XXX')
-                        .reply(401);
-                    return ncb.balance('foo', function(err, balance) {
-                                           assert(err == 'invalid status 401 received');
-                                           done();
+                    var scope = nockit('POST', 'transactions/send_money', n_body);
+                    scope.reply(401);
+                    return ncb.send_btc('foo', t_to, t_amount, t_note, function(err, id) {
+                                                assert(err == 'invalid status 401 received');
+                                                done();
                                        });
                 });
 
